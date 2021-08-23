b3050dcdf40fd7c349a8d2cd6d250df994e548f6
vinicius2405 <vinicius.alexandretrentin@gmail.com>
2021-08-23 20:00:51 -0300

    Site com css e 3 eventos

--- ceep/src/components/CardNota/CardNota.jsx
+++ ceep/src/components/CardNota/CardNota.jsx
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
-import "./CardNota.css"
+import React, { Component } from "react";
+import "./CardNota.css";
 class CardNota extends Component {
-    render() {
-        return (
-            <section className ="card-nota">
-                <header>
-                    <h3 className="card-nota_item">Titulo</h3>
-                </header>
-                <p>Escreva sua nota</p>
-            </section>);
-    }
+  render() {
+    return (
+      <section className="card-nota">
+        <header className="card-nota_cabecalho">
+          <h3 className="card-nota_titulo">Título</h3>
+        </header>
+        <p className="card-nota_texto">Escreva sua nota</p>
+      </section>
+    );
+  }
 }
 
 export default CardNota;
-
